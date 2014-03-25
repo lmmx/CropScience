e2431ee9133c8278d33924d774bf9d9fe247516f
Louis Maddox <maddo16@hotmail.co.uk>
2014-03-25 14:20:41 +0000

    Minimal variable declaration

--- Javascripts/CropPrint Menu.js
+++ Javascripts/CropPrint Menu.js
@@ -1,1 +1,6 @@
 app.addSubMenu({cName:"Crop and Print",cParent:"File", nPos:24});
+
+var d = ['Art','Bleed','Trim','Crop'];
+var o;
+function n() { return this.numPages; }
+function a() { return this; }
--- Javascripts/CropPrintBriefBioinfo.js
+++ Javascripts/CropPrintBriefBioinfo.js
@@ -1,6 +1,1 @@
-var d = ['Art','Bleed','Trim','Crop'];
-var o;
-function n() { return this.numPages; }
-function a() { return this; }
-
 app.addMenuItem({cName:"Crop for Briefings in Bioinfo.",cParent:"Crop and Print", cExec:"for(var p=0;p<n();p++){o=a().getPageBox(d[3],p);o[0]+=62.6;o[1]-=51.5;o[2]-=62;o[3]+=28;for(b=0;b<4;b++){a().setPageBoxes(d[b],p,p,o)}};this.print();"});
