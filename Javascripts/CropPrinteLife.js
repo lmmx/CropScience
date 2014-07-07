2508646c171f2ad8e4d13239a4797d8e4aeaa060
Louis <naivelocus@gmail.com>
2014-07-08 00:28:00 +0100

    eLife

---
+++ Javascripts/CropPrinteLife.js
@@ -0,0 +1,1 @@
+app.addMenuItem({cName:"Crop for eLife",cParent:"Crop and Print", cExec:"for(var p=0;p<n();p++){o=a().getPageBox('BBox',p);o[0]+=1;o[1]-=22;o[2]-=8;o[3]+=35;if(p==0){o[0]+=130;o[1]-=48;}for(b=0;b<4;b++){a().setPageBoxes(d[b],p,p,o)}};this.print();"});
