779d062bbcd48e3d97b186725f15ac93a6b1805a
Louis <naivelocus@gmail.com>
2014-07-08 15:02:49 +0100

    MolSysBio outside main folder

---
+++ Javascripts/CropPrintMolSysBio.js
@@ -0,0 +1,1 @@
+app.addMenuItem({cName:"Crop for Molecular Systems Biology",cParent:"Crop and Print", cExec:"for(var p=0;p<n();p++){o=a().getPageBox('BBox',p);o[1]-=43;o[3]+=22;if (p==0){o[1]+=8};for(b=0;b<4;b++){a().setPageBoxes(d[b],p,p,o)}};this.print();"});
