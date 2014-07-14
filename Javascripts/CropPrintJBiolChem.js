1630667356d4a076597d0d2af96c93f8a6a3e107
Louis <naivelocus@gmail.com>
2014-07-14 13:36:49 +0100

    JBC was overshooting RHS margin

--- Javascripts/CropPrintJBiolChem.js
+++ Javascripts/CropPrintJBiolChem.js
@@ -1,1 +1,1 @@
-app.addMenuItem({cName:"Crop for J. Biol. Chem.",cParent:"Crop and Print", cExec:"for(var p=0;p<n();p++){o=a().getPageBox('BBox',p);o[1]-=20;o[2]-=40;if(p==0){o[1]-=6;o[3]+=16;};for(b=0;b<4;b++){a().setPageBoxes(d[b],p,p,o)}};this.print();"});
+app.addMenuItem({cName:"Crop for J. Biol. Chem.",cParent:"Crop and Print", cExec:"for(var p=0;p<n();p++){o=a().getPageBox('BBox',p);o[1]-=20;o[2]-=30;if(p==0){o[1]-=6;o[3]+=16;};for(b=0;b<4;b++){a().setPageBoxes(d[b],p,p,o)}};this.print();"});
